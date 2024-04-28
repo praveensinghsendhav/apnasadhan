9959057b7e4bb187f0b452f6923286e9deb30bd7
praveensinghsendhav <praveensinghsendhav2002@gmail.com>
2024-04-28 15:11:35 +0530

    adding cloud database

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose"); 
 
-mongoose.connect("mongodb://localhost:27017/ApnaSadhan").then(()=>{
+mongoose.connect("mongodb+srv://apnasadhan:praveen%40as1@apnasadhan.fstmz0q.mongodb.net/ApnaSadhan").then(()=>{
   console.log("database connected succesfully");
 }).catch((e)=>{
   console.log(e);
